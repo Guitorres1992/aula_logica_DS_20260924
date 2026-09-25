1d5e74a1d0cc900d00f4782cf4c6c3efff366d53
Guilherme Torres <guitorres.vieira1992@gmail.com>
2026-09-24 22:39:01 -0300

    Arquivo_Novo

--- main.c
+++ main.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    char nome[20];
-    int idade;
+int main() {
+    int x, y, soma;
+    printf("Digite o valor de X: ");
+    scanf("%d", &x);
+    printf("Digite o valor de Y: ");
+    scanf("%d", &y);
 
-    printf("Ola! Mundo o seu nome: \n");
-    scanf("%s", nome);
-
-    printf("Qual a sua idade: \n");
-    scanf("%d", &idade);
-
-    printf("Nome: %s\n", nome);
-    printf("Idade: %d\n", idade);
-
-    if (idade >= 18)
-    {
-        printf("Voce e maior de idade!\n");
-    }
-    else
-    {
-        printf("Voce e menor de idade!\n");
-    }
-
+    soma = x + y;
+    printf("SOMA = %d\n", soma);
     return 0;
 }
+
+
+
+
+
+
